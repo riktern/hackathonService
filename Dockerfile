f4dfc9d8de8d1ef7c7c7bc6045463c2ce5d9981f
seku <seku345@yandex.ru>
2024-12-14 16:11:41 +0300

    updated models, init.sql & relations

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 # Базовый образ. По умолчанию берется из https://hub.docker.com/_/python
 FROM python:3.12-slim
 
-# Выполнить команду в контейрене.
-RUN apt-get update && apt-get install -y gcc python3-dev
-
 # Поменять рабочую директорию. Если ее нет, создать ее.
 WORKDIR /app
 
--- persistent/db/role.py
+++ persistent/db/role.py
@@ -1,5 +1,5 @@
 from persistent.db.base import Base, WithId
-from sqlalchemy import Column, Text, Integer, Boolean, DateTime, Float
+from sqlalchemy import Column, Text, Integer, Boolean, DateTime, Float, UUID, ForeignKey
 from sqlalchemy.dialects.postgresql import ARRAY
 from datetime import datetime
 
@@ -8,3 +8,4 @@ class Role(Base, WithId):
     __tablename__ = "role"
 
     name = Column(Text, nullable=False)
+    hacker_id = Column(UUID(as_uuid=True), ForeignKey("hacker.id"), nullable=False)
